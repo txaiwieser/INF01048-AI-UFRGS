b50e0539f4339235a13cece1922d53836181365a
Txai Wieser <txaidw@gmail.com>
2021-04-17 14:57:15 -0300

    Add multiple variable implementation

--- t1/p3/gradient_based_optimization.py
+++ t1/p3/gradient_based_optimization.py
@@ -22,7 +22,7 @@ rawCSVData = np.genfromtxt('/Users/txaiwieser/GitHub/INF01048-AI-UFRGS/t1/p3/hou
 #Extrair colunas para análise
 indexOfGrLivArea = 46
 indexOfSalePrice = 80
-indexOfSalePrice = 17
+indexOfOverallQual = 17
 indexOfOverallCond = 18
 indexOfGarageArea = 62
 indexOfYearBuilt = 19
@@ -124,54 +124,12 @@ def one_param_step_gradient(theta_0_current, theta_1_current, data, alpha):
     return theta_0_final, theta_1_final
 
 def two_param_step_gradient(theta_0_current, theta_1_current, theta_2_current, data, alpha):
-    theta_0_updated = 0
-    theta_1_updated = 0
-    theta_2_updated = 0
-
-    def h(x1, x2):
-      return theta_0_current + theta_1_current * x1 + theta_2_current * x2
-
-    x1 = np.array(data[:,0])
-    x1 = np.array(data[:,1])
-    y = np.array(data[:,2])
-    n = len(x)
-
-    for i in range(n):
-      theta_0_updated += h(x1[i], x2[i]) - y[i]
-      theta_1_updated += (h(x1[i], x2) - y[i]) * x1[i]
-
-    theta_0_updated *= 2/n
-    theta_1_updated *= 2/n
-    
-    theta_0_final = theta_0_current - alpha * theta_0_updated
-    theta_1_final = theta_1_current - alpha * theta_1_updated
-    
-    return theta_0_final, theta_1_final # NAO IMPLEMENTEI AINDA
+    # NAO IMPLEMENTEI AINDA
+    return 0
 
 def five_param_step_gradient(theta_0_current, theta_1_current, theta_2_current, theta_3_current, theta_4_current, theta_5_current, data, alpha):
-    theta_0_updated = 0
-    theta_1_updated = 0
-    theta_2_updated = 0
-
-    def h(x1, x2):
-      return theta_0_current + theta_1_current * x1 + theta_2_current * x2
-
-    x1 = np.array(data[:,0])
-    x1 = np.array(data[:,1])
-    y = np.array(data[:,2])
-    n = len(x)
-
-    for i in range(n):
-      theta_0_updated += h(x1[i], x2[i]) - y[i]
-      theta_1_updated += (h(x1[i], x2) - y[i]) * x1[i]
-
-    theta_0_updated *= 2/n
-    theta_1_updated *= 2/n
-    
-    theta_0_final = theta_0_current - alpha * theta_0_updated
-    theta_1_final = theta_1_current - alpha * theta_1_updated
-    
-    return theta_0_final, theta_1_final # NAO IMPLEMENTEI AINDA
+   # NAO IMPLEMENTEI AINDA
+   return 0
 
 def one_param_gradient_descent(data, starting_theta_0, starting_theta_1, learning_rate, num_iterations):
     # valores iniciais
@@ -191,7 +149,7 @@ def one_param_gradient_descent(data, starting_theta_0, starting_theta_1, learnin
         theta_0, theta_1 = one_param_step_gradient(theta_0, theta_1, data, alpha=0.0001)
         theta_0_progress.append(theta_0)
         theta_1_progress.append(theta_1)
-        
+
     return [theta_0, theta_1, cost_graph, theta_0_progress, theta_1_progress]
 
 def two_param_gradient_descent(data, starting_theta_0, starting_theta_1, starting_theta_2, learning_rate, num_iterations):
@@ -289,6 +247,3 @@ def five_param_compute():
 
     #Imprimir erro com os parâmetros otimizados
     print ('Erro quadratico medio: ', five_param_compute_cost(theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, data))
-
-
-    
