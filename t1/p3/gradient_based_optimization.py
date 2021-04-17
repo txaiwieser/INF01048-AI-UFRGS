ac19ed2be903e87e11edace41022cb5077fa23da
Txai Wieser <txaidw@gmail.com>
2021-04-17 12:48:54 -0300

    Change name

--- t1/p3/gradient_based_optimization.py
+++ t1/p3/gradient_based_optimization.py
@@ -8,116 +8,175 @@ DEBUG = False
 def debugPrint(str):
     if DEBUG: print("DEBUG: " + str)
 
+def normalizeArray(arr):
+    min = arr.min()
+    max = arr.max()
+    def normalize(x):
+        return (x - min) / (max - min)
+    return normalize(arr)
+
 # MAIN
 
 rawCSVData = np.genfromtxt('/Users/txaiwieser/GitHub/INF01048-AI-UFRGS/t1/p3/house_prices_train.csv', delimiter=',')
 
 #Extrair colunas para análise
-indexOfGrLivArea = 46 #columnLabels.index("GrLivArea")
-indexOfSalePrice = 80 #columnLabels.index("SalePrice")
+indexOfGrLivArea = 46
+indexOfSalePrice = 80
+indexOfSalePrice = 17
+indexOfOverallCond = 18
+indexOfGarageArea = 62
+indexOfYearBuilt = 19
+
 grLivAreas = np.array(rawCSVData[1:,indexOfGrLivArea])
 salePrices = np.array(rawCSVData[1:,indexOfSalePrice])
+overallQuals = np.array(rawCSVData[1:,indexOfOverallQual])
+overallConds = np.array(rawCSVData[1:,indexOfOverallCond])
+garageAreas = np.array(rawCSVData[1:,indexOfGarageArea])
+yearsBuilt = np.array(rawCSVData[1:,indexOfYearBuilt])
 
-# print(grLivAreas)
-# print(salePrices)
+normalizedGrLivAreas = normalizeArray(grLivAreas)
+normalizedOverallQuals = normalizeArray(overallQuals)
+normalizedOverallConds = normalizeArray(overallConds)
+normalizedGarageAreas = normalizeArray(garageAreas)
+normalizedYearsBuilt = normalizeArray(yearsBuilt)
 
-minGrLivAreas = grLivAreas.min()
-maxGrLivAreas = grLivAreas.max()
-def normalizeGrLivAreasDivider(x):
-  return (x - minGrLivAreas) / (maxGrLivAreas - minGrLivAreas)
-normalizedGrLivAreas = normalizeGrLivAreasDivider(grLivAreas)
-
-data = np.array([normalizedGrLivAreas, salePrices])
-
-# #Gráfico dos dados
-# plt.figure(figsize=(10, 6))
-# plt.scatter(x,y)
-# plt.xlabel('Horas de estudo')
-# plt.ylabel('Nota final')
-# plt.title('Dados')
-# plt.show()
+oneParamData = np.array([normalizedGrLivAreas, salePrices])
+twoParamData = np.array([normalizedGrLivAreas, normalizedOverallQuals, salePrices])
+fiveParamData = np.array([normalizedGrLivAreas, normalizedOverallQuals, normalizedOverallConds, normalizedGarageAreas, normalizedYearsBuilt, salePrices])
 
 #### Definição da função de custo
 
-def compute_cost(theta_0, theta_1, data):
-    """
-    Calcula o erro quadratico medio
-    
-    Args:
-        theta_0 (float): intercepto da reta 
-        theta_1 (float): inclinacao da reta
-        data (np.array): matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    
-    Retorna:
-        float: o erro quadratico medio
-    """
+def one_param_compute_cost(theta_0, theta_1, data):
     total_cost = 0
-    ### SEU CODIGO AQUI
 
-    def h(x):
-      return theta_0 + theta_1 * x
+    def h(x1, x2):
+      return theta_0 + theta_1 * x1
 
-    x = np.array(data[:,0])
+    x1 = np.array(data[:,0])
     y = np.array(data[:,1])
-    n = len(x)
+    n = len(y)
+    
     for i in range(n):
-      total_cost += pow(h(x[i]) - y[i], 2)
+      total_cost += pow(h(x1[i]) - y[i], 2)
+
+    total_cost /= n
+    return total_cost
+
+def two_param_compute_cost(theta_0, theta_1, theta_2, data):
+    total_cost = 0
+
+    def h(x1, x2):
+      return theta_0 + theta_1 * x1 + theta_2 * x2
+
+    x1 = np.array(data[:,0])
+    x2 = np.array(data[:,1])
+    y = np.array(data[:,2])
+    n = len(y)
+
+    for i in range(n):
+      total_cost += pow(h(x1[i], x2[i]) - y[i], 2)
+
+    total_cost /= n
+    return total_cost
+
+def five_param_compute_cost(theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, data):
+    total_cost = 0
+
+    def h(x1, x2, x3, x4, x5):
+      return theta_0 + theta_1 * x1 + theta_2 * x2 + theta_3 * x3 + theta_4 * x4 + theta_5 * x5
+
+    x1 = np.array(data[:,0])
+    x2 = np.array(data[:,1])
+    x3 = np.array(data[:,2])
+    x4 = np.array(data[:,3])
+    x5 = np.array(data[:,4])
+    y = np.array(data[:,5])
+    n = len(y)
+
+    for i in range(n):
+      total_cost += pow(h(x1[i], x2[i], x3[i], x4[i], x5[i]) - y[i], 2)
 
     total_cost /= n
     return total_cost
 
 #### Define as funções de Gradiente Descendente
 
-def step_gradient(theta_0_current, theta_1_current, data, alpha):
-    """Calcula um passo em direção ao EQM mínimo
-    
-    Args:
-        theta_0_current (float): valor atual de theta_0
-        theta_1_current (float): valor atual de theta_1
-        data (np.array): vetor com dados de treinamento (x,y)
-        alpha (float): taxa de aprendizado / tamanho do passo 
-    
-    Retorna:
-        tupla: (theta_0, theta_1) os novos valores de theta_0, theta_1
-    """
-    
+def one_param_step_gradient(theta_0_current, theta_1_current, data, alpha):
     theta_0_updated = 0
     theta_1_updated = 0
     
-    ### SEU CODIGO AQUI
-    def h(x):
-      return theta_0_current + theta_1_current * x
+    def h(x1):
+      return theta_0_current + theta_1_current * x1
 
-    x = np.array(data[:,0])
+    x1 = np.array(data[:,0])
     y = np.array(data[:,1])
     n = len(x)
     for i in range(n):
-      theta_0_updated += h(x[i]) - y[i]
-      theta_1_updated += (h(x[i]) - y[i]) * x[i]
+      theta_0_updated += h(x1[i]) - y[i]
+      theta_1_updated += (h(x1[i]) - y[i]) * x1[i]
 
     theta_0_updated *= 2/n
     theta_1_updated *= 2/n
-
-    return theta_0_current - alpha * theta_0_updated, theta_1_current - alpha * theta_1_updated
-
-def gradient_descent(data, starting_theta_0, starting_theta_1, learning_rate, num_iterations):
-    """executa a descida do gradiente
     
-    Args:
-        data (np.array): dados de treinamento, x na coluna 0 e y na coluna 1
-        starting_theta_0 (float): valor inicial de theta0 
-        starting_theta_1 (float): valor inicial de theta1
-        learning_rate (float): hyperparâmetro para ajustar o tamanho do passo durante a descida do gradiente
-        num_iterations (int): hyperparâmetro que decide o número de iterações que cada descida de gradiente irá executar
+    theta_0_final = theta_0_current - alpha * theta_0_updated
+    theta_1_final = theta_1_current - alpha * theta_1_updated
     
-    Retorna:
-        list : os primeiros dois parâmetros são o Theta0 e Theta1, que armazena o melhor ajuste da curva. O terceiro e quarto parâmetro, são vetores com o histórico dos valores para Theta0 e Theta1.
-    """
+    return theta_0_final, theta_1_final
 
+def two_param_step_gradient(theta_0_current, theta_1_current, theta_2_current, data, alpha):
+    theta_0_updated = 0
+    theta_1_updated = 0
+    theta_2_updated = 0
+
+    def h(x1, x2):
+      return theta_0_current + theta_1_current * x1 + theta_2_current * x2
+
+    x1 = np.array(data[:,0])
+    x1 = np.array(data[:,1])
+    y = np.array(data[:,2])
+    n = len(x)
+
+    for i in range(n):
+      theta_0_updated += h(x1[i], x2[i]) - y[i]
+      theta_1_updated += (h(x1[i], x2) - y[i]) * x1[i]
+
+    theta_0_updated *= 2/n
+    theta_1_updated *= 2/n
+    
+    theta_0_final = theta_0_current - alpha * theta_0_updated
+    theta_1_final = theta_1_current - alpha * theta_1_updated
+    
+    return theta_0_final, theta_1_final # NAO IMPLEMENTEI AINDA
+
+def five_param_step_gradient(theta_0_current, theta_1_current, theta_2_current, theta_3_current, theta_4_current, theta_5_current, data, alpha):
+    theta_0_updated = 0
+    theta_1_updated = 0
+    theta_2_updated = 0
+
+    def h(x1, x2):
+      return theta_0_current + theta_1_current * x1 + theta_2_current * x2
+
+    x1 = np.array(data[:,0])
+    x1 = np.array(data[:,1])
+    y = np.array(data[:,2])
+    n = len(x)
+
+    for i in range(n):
+      theta_0_updated += h(x1[i], x2[i]) - y[i]
+      theta_1_updated += (h(x1[i], x2) - y[i]) * x1[i]
+
+    theta_0_updated *= 2/n
+    theta_1_updated *= 2/n
+    
+    theta_0_final = theta_0_current - alpha * theta_0_updated
+    theta_1_final = theta_1_current - alpha * theta_1_updated
+    
+    return theta_0_final, theta_1_final # NAO IMPLEMENTEI AINDA
+
+def one_param_gradient_descent(data, starting_theta_0, starting_theta_1, learning_rate, num_iterations):
     # valores iniciais
     theta_0 = starting_theta_0
     theta_1 = starting_theta_1
-
     
     # variável para armazenar o custo ao final de cada step_gradient
     cost_graph = []
@@ -127,46 +186,109 @@ def gradient_descent(data, starting_theta_0, starting_theta_1, learning_rate, nu
     theta_1_progress = []
     
     # Para cada iteração, obtem novos (Theta0,Theta1) e calcula o custo (EQM)
-    num_iterations = 10
     for i in range(num_iterations):
-        cost_graph.append(compute_cost(theta_0, theta_1, data))
-        theta_0, theta_1 = step_gradient(theta_0, theta_1, data, alpha=0.0001)
+        cost_graph.append(one_param_compute_cost(theta_0, theta_1, data))
+        theta_0, theta_1 = one_param_step_gradient(theta_0, theta_1, data, alpha=0.0001)
         theta_0_progress.append(theta_0)
         theta_1_progress.append(theta_1)
         
     return [theta_0, theta_1, cost_graph, theta_0_progress, theta_1_progress]
 
+def two_param_gradient_descent(data, starting_theta_0, starting_theta_1, starting_theta_2, learning_rate, num_iterations):
+    # valores iniciais
+    theta_0 = starting_theta_0
+    theta_1 = starting_theta_1
+    theta_2 = starting_theta_2
+
+    # variável para armazenar o custo ao final de cada step_gradient
+    cost_graph = []
+    
+    # vetores para armazenar os valores de Theta0 e Theta1 apos cada iteração de step_gradient (pred = Theta1*x + Theta0)
+    theta_0_progress = []
+    theta_1_progress = []
+    theta_2_progress = []
+    
+    # Para cada iteração, obtem novos (Theta0, Theta1) e calcula o custo (EQM)
+    for i in range(num_iterations):
+        cost_graph.append(two_param_oncompute_cost(theta_0, theta_1, theta_2, data))
+        theta_0, theta_1 = two_param_step_gradient(theta_0, theta_1, theta_2, data, alpha=0.0001)
+        theta_0_progress.append(theta_0)
+        theta_1_progress.append(theta_1)
+        theta_2_progress.append(theta_2)
+        
+    return [theta_0, theta_1, theta_2, cost_graph, theta_0_progress, theta_1_progress, theta_2_progress]
+
+def five_param_gradient_descent(data, starting_theta_0, starting_theta_1, starting_theta_2, starting_theta_3, starting_theta_4, starting_theta_5, learning_rate, num_iterations):
+    # valores iniciais
+    theta_0 = starting_theta_0
+    theta_1 = starting_theta_1
+    theta_2 = starting_theta_2
+    theta_3 = starting_theta_3
+    theta_4 = starting_theta_4
+    theta_5 = starting_theta_5
+
+    # variável para armazenar o custo ao final de cada step_gradient
+    cost_graph = []
+    
+    # vetores para armazenar os valores de Theta0 e Theta1 apos cada iteração de step_gradient (pred = Theta1*x + Theta0)
+    theta_0_progress = []
+    theta_1_progress = []
+    theta_2_progress = []
+    theta_3_progress = []
+    theta_4_progress = []
+    theta_5_progress = []
+    
+    # Para cada iteração, obtem novos (Theta0, Theta1) e calcula o custo (EQM)
+    for i in range(num_iterations):
+        cost_graph.append(five_param_oncompute_cost(theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, data))
+        theta_0, theta_1 = five_param_step_gradient(theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, data, alpha=0.0001)
+        theta_0_progress.append(theta_0)
+        theta_1_progress.append(theta_1)
+        theta_2_progress.append(theta_2)
+        theta_3_progress.append(theta_3)
+        theta_4_progress.append(theta_4)
+        theta_5_progress.append(theta_5)
+        
+    return [theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, cost_graph, theta_0_progress, theta_1_progress, theta_2_progress, theta_3_progress, theta_4_progress, theta_5_progress]
+
 #### Executa a função gradient_descent() para obter os parâmetros otimizados, Theta0 e Theta1.
 
 num_iterations = 100
-theta_0, theta_1, cost_graph, theta_0_progress, theta_1_progress = gradient_descent(data, starting_theta_0=0, starting_theta_1=0, learning_rate=0, num_iterations=num_iterations)
 
-#Imprimir parâmetros otimizados
-print ('Theta_0: ', theta_0)
-print ('Theta_1: ', theta_1)
+def one_param_compute():
+    theta_0, theta_1, cost_graph, theta_0_progress, theta_1_progress = one_param_gradient_descent(data, starting_theta_0=0, starting_theta_1=0, learning_rate=0, num_iterations=num_iterations)
 
-#Imprimir erro com os parâmetros otimizados
-print ('Erro quadratico medio: ', compute_cost(theta_0, theta_1, data))
+    #Imprimir parâmetros otimizados
+    print ('Theta_0: ', theta_0)
+    print ('Theta_1: ', theta_1)
 
-#### Gráfico do custo por iteração
+    #Imprimir erro com os parâmetros otimizados
+    print ('Erro quadratico medio: ', compute_cost(theta_0, theta_1, data))
 
-# # plt.figure(figsize=(10, 6))
-# # plt.plot(cost_graph)
-# # plt.xlabel('No. de interações')
-# # plt.ylabel('Custo')
-# # plt.title('Custo por iteração')
-# # plt.show()
+def two_param_compute():
+    theta_0, theta_1, theta_2, cost_graph, theta_0_progress, theta_1_progress, theta_2_progress = two_param_gradient_descent(data, starting_theta_0=0, starting_theta_1=0, learning_rate=0, num_iterations=num_iterations)
 
-# #### Gráfico de linha com melhor ajuste
+    #Imprimir parâmetros otimizados
+    print ('Theta_0: ', theta_0)
+    print ('Theta_1: ', theta_1)
+    print ('Theta_2: ', theta_2)
 
-# # #Gráfico de dispersão do conjunto de dados
-# # plt.figure(figsize=(10, 6))
-# # plt.scatter(x, y)
-# # #Valores preditos de y
-# # pred = theta_1 * x + theta_0
-# # #Gráfico de linha do melhor ajuste
-# # plt.plot(x, pred, c='r')
-# # plt.xlabel('Horas de estudo')
-# # plt.ylabel('Nota final')
-# # plt.title('Melhor ajuste')
-# # plt.show()
+    #Imprimir erro com os parâmetros otimizados
+    print ('Erro quadratico medio: ', two_param_compute_cost(theta_0, theta_1, theta_2, data))
+
+def five_param_compute():
+    theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, cost_graph, theta_0_progress, theta_1_progress, theta_2_progress, theta_3_progress, theta_4_progress, theta_5_progress = two_param_gradient_descent(data, starting_theta_0=0, starting_theta_1=0, learning_rate=0, num_iterations=num_iterations)
+
+    #Imprimir parâmetros otimizados
+    print ('Theta_0: ', theta_0)
+    print ('Theta_1: ', theta_1)
+    print ('Theta_2: ', theta_2)
+    print ('Theta_3: ', theta_3)
+    print ('Theta_4: ', theta_4)
+    print ('Theta_5: ', theta_5)
+
+    #Imprimir erro com os parâmetros otimizados
+    print ('Erro quadratico medio: ', five_param_compute_cost(theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, data))
+
+
+    
