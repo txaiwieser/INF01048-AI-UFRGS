ad6a7b04bbf8ce4847dfc185e13692794fbc6720
Txai Wieser <txaidw@gmail.com>
2021-04-17 12:06:40 -0300

    First questions

--- t1/p3/gradient_based_optimization.py
+++ t1/p3/gradient_based_optimization.py
@@ -10,11 +10,24 @@ def debugPrint(str):
 
 # MAIN
 
-data = np.genfromtxt('/Users/txaiwieser/GitHub/INF01048-AI-UFRGS/t1/p3/house_prices_train.csv', delimiter=',')
+rawCSVData = np.genfromtxt('/Users/txaiwieser/GitHub/INF01048-AI-UFRGS/t1/p3/house_prices_train.csv', delimiter=',')
 
-# #Extrair colunas para análise
-# x = np.array(data[:,0])
-# y = np.array(data[:,1])
+#Extrair colunas para análise
+indexOfGrLivArea = 46 #columnLabels.index("GrLivArea")
+indexOfSalePrice = 80 #columnLabels.index("SalePrice")
+grLivAreas = np.array(rawCSVData[1:,indexOfGrLivArea])
+salePrices = np.array(rawCSVData[1:,indexOfSalePrice])
+
+# print(grLivAreas)
+# print(salePrices)
+
+minGrLivAreas = grLivAreas.min()
+maxGrLivAreas = grLivAreas.max()
+def normalizeGrLivAreasDivider(x):
+  return (x - minGrLivAreas) / (maxGrLivAreas - minGrLivAreas)
+normalizedGrLivAreas = normalizeGrLivAreasDivider(grLivAreas)
+
+data = np.array([normalizedGrLivAreas, salePrices])
 
 # #Gráfico dos dados
 # plt.figure(figsize=(10, 6))
@@ -53,18 +66,6 @@ def compute_cost(theta_0, theta_1, data):
     total_cost /= n
     return total_cost
 
-#### Teste do Erro Quadratico Medio
-
-theta_0_test = 0
-theta_1_test = 0
-# comparacao de floats com tolerancia 1E-11
-if abs(compute_cost(theta_0_test, theta_1_test, data) - 5565.107834483211) < 1E-11:
-  print("Erro Quadratico Medio passou no teste")
-  print(abs(compute_cost(theta_0_test, theta_1_test, data)))
-else:
-  print("ERRO NO CALCULO DO ERRO QUADRATICO MEDIO!")
-  print(abs(compute_cost(theta_0_test, theta_1_test, data)))
-
 #### Define as funções de Gradiente Descendente
 
 def step_gradient(theta_0_current, theta_1_current, data, alpha):
@@ -98,28 +99,6 @@ def step_gradient(theta_0_current, theta_1_current, data, alpha):
     theta_1_updated *= 2/n
 
     return theta_0_current - alpha * theta_0_updated, theta_1_current - alpha * theta_1_updated
-
-### Teste da funcao step_gradient
-
-# dataset copiado do Quiz de Otimizacao Continua
-other_data = np.array([
-                 [1, 3],
-                 [2, 4],
-                 [3, 4], 
-                 [4, 2]
-             ])
-
-new_theta0, new_theta1 = step_gradient(1, 1, other_data, alpha=0.1)
-# comparacao de floats com tolerancia 1E-11
-if abs(new_theta0 - 0.95) < 1E-11:
-  print("Atualizacao de theta0 OK")
-else:
-  print("ERRO NA ATUALIZACAO DE theta0!")
-
-if abs(new_theta1 - 0.55) < 1E-11:
-  print("Atualizacao de theta1 OK")
-else:
-  print("ERRO NA ATUALIZACAO DE theta1!")
 
 def gradient_descent(data, starting_theta_0, starting_theta_1, learning_rate, num_iterations):
     """executa a descida do gradiente
@@ -159,36 +138,35 @@ def gradient_descent(data, starting_theta_0, starting_theta_1, learning_rate, nu
 
 #### Executa a função gradient_descent() para obter os parâmetros otimizados, Theta0 e Theta1.
 
-
-
-theta_0, theta_1, cost_graph, theta_0_progress, theta_1_progress = gradient_descent(data, starting_theta_0=0, starting_theta_1=0, learning_rate=0, num_iterations=10)
+num_iterations = 100
+theta_0, theta_1, cost_graph, theta_0_progress, theta_1_progress = gradient_descent(data, starting_theta_0=0, starting_theta_1=0, learning_rate=0, num_iterations=num_iterations)
 
 #Imprimir parâmetros otimizados
-print ('Theta_0 otimizado: ', theta_0)
-print ('Theta_1 otimizado: ', theta_1)
+print ('Theta_0: ', theta_0)
+print ('Theta_1: ', theta_1)
 
 #Imprimir erro com os parâmetros otimizados
-print ('Custo minimizado: ', compute_cost(theta_0, theta_1, data))
+print ('Erro quadratico medio: ', compute_cost(theta_0, theta_1, data))
 
 #### Gráfico do custo por iteração
 
-# plt.figure(figsize=(10, 6))
-# plt.plot(cost_graph)
-# plt.xlabel('No. de interações')
-# plt.ylabel('Custo')
-# plt.title('Custo por iteração')
-# plt.show()
+# # plt.figure(figsize=(10, 6))
+# # plt.plot(cost_graph)
+# # plt.xlabel('No. de interações')
+# # plt.ylabel('Custo')
+# # plt.title('Custo por iteração')
+# # plt.show()
 
-#### Gráfico de linha com melhor ajuste
+# #### Gráfico de linha com melhor ajuste
 
-# #Gráfico de dispersão do conjunto de dados
-# plt.figure(figsize=(10, 6))
-# plt.scatter(x, y)
-# #Valores preditos de y
-# pred = theta_1 * x + theta_0
-# #Gráfico de linha do melhor ajuste
-# plt.plot(x, pred, c='r')
-# plt.xlabel('Horas de estudo')
-# plt.ylabel('Nota final')
-# plt.title('Melhor ajuste')
-# plt.show()
+# # #Gráfico de dispersão do conjunto de dados
+# # plt.figure(figsize=(10, 6))
+# # plt.scatter(x, y)
+# # #Valores preditos de y
+# # pred = theta_1 * x + theta_0
+# # #Gráfico de linha do melhor ajuste
+# # plt.plot(x, pred, c='r')
+# # plt.xlabel('Horas de estudo')
+# # plt.ylabel('Nota final')
+# # plt.title('Melhor ajuste')
+# # plt.show()
