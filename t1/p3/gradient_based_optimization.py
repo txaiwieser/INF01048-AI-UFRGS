0ea322baa929e0ff9aeb590f54375695416979d8
Txai Wieser <txaidw@gmail.com>
2021-04-17 17:29:08 -0300

    Update multiple variable gradients

--- t1/p3/gradient_based_optimization.py
+++ t1/p3/gradient_based_optimization.py
@@ -112,7 +112,9 @@ def one_param_step_gradient(theta_0_current, theta_1_current, data, alpha):
     y = np.array(data[:,1])
     n = len(y)
     for i in range(n):
-      theta_0_updated += h(x1[i]) - y[i]
+      # 2 (a + b x1 + c x2 - y)
+      theta_0_updated += (h(x1[i]) - y[i])
+      # 2 x1 (a + b x1 + c x2 - y)
       theta_1_updated += (h(x1[i]) - y[i]) * x1[i]
 
     theta_0_updated *= 2/n
@@ -124,12 +126,84 @@ def one_param_step_gradient(theta_0_current, theta_1_current, data, alpha):
     return theta_0_final, theta_1_final
 
 def two_param_step_gradient(theta_0_current, theta_1_current, theta_2_current, data, alpha):
-    # NAO IMPLEMENTEI AINDA
-    return 0, 0, 0
+    theta_0_updated = 0
+    theta_1_updated = 0
+    theta_2_updated = 0
+    
+    def h(x1, x2):
+      return theta_0_current + theta_1_current * x1 + theta_2_current * x2
+
+    x1 = np.array(data[:,0])
+    x2 = np.array(data[:,1])
+    y = np.array(data[:,2])
+    n = len(y)
+
+    for i in range(n):
+      # 2 (a + b x1 + c x2 - y)
+      theta_0_updated += (h(x1[i], x2[i]) - y[i])
+      # 2 x1 (a + b x1 + c x2 - y)
+      theta_1_updated += (h(x1[i], x2[i]) - y[i]) * x1[i]
+      # 2 x2 (a + b x1 + c x2 - y)
+      theta_2_updated += (h(x1[i], x2[i]) - y[i]) * x2[i]
+
+    theta_0_updated *= 2/n
+    theta_1_updated *= 2/n
+    theta_2_updated *= 2/n
+    
+    theta_0_final = theta_0_current - alpha * theta_0_updated
+    theta_1_final = theta_1_current - alpha * theta_1_updated
+    theta_2_final = theta_2_current - alpha * theta_2_updated
+    
+    return theta_0_final, theta_1_final, theta_2_final
 
 def five_param_step_gradient(theta_0_current, theta_1_current, theta_2_current, theta_3_current, theta_4_current, theta_5_current, data, alpha):
-   # NAO IMPLEMENTEI AINDA
-   return 0, 0, 0, 0, 0, 0
+    theta_0_updated = 0
+    theta_1_updated = 0
+    theta_2_updated = 0
+    theta_3_updated = 0
+    theta_4_updated = 0
+    theta_5_updated = 0
+    
+    def h(x1, x2, x3, x4, x5):
+      return theta_0_current + theta_1_current * x1 + theta_2_current * x2 + theta_3_current * x3 + theta_4_current * x4 + theta_5_current * x5
+
+    x1 = np.array(data[:,0])
+    x2 = np.array(data[:,1])
+    x3 = np.array(data[:,2])
+    x4 = np.array(data[:,3])
+    x5 = np.array(data[:,4])
+    y = np.array(data[:,5])
+    n = len(y)
+
+    for i in range(n):
+      # 2 (a + b x1 + c x2 + d x3 + e x4 + f x5 - y)
+      theta_0_updated += (h(x1[i], x2[i], x3[i], x4[i], x5[i]) - y[i])
+      # 2 x1 (a + b x1 + c x2 + d x3 + e x4 + f x5 - y)
+      theta_1_updated += (h(x1[i], x2[i], x3[i], x4[i], x5[i]) - y[i]) * x1[i]
+      # 2 x2 (a + b x1 + c x2 + d x3 + e x4 + f x5 - y)
+      theta_2_updated += (h(x1[i], x2[i], x3[i], x4[i], x5[i]) - y[i]) * x2[i]
+      # 2 x3 (a + b x1 + c x2 + d x3 + e x4 + f x5 - y)
+      theta_3_updated += (h(x1[i], x2[i], x3[i], x4[i], x5[i]) - y[i]) * x3[i]
+      # 2 x4 (a + b x1 + c x2 + d x3 + e x4 + f x5 - y)
+      theta_4_updated += (h(x1[i], x2[i], x3[i], x4[i], x5[i]) - y[i]) * x4[i]
+      # 2 x5 (a + b x1 + c x2 + d x3 + e x4 + f x5 - y)
+      theta_5_updated += (h(x1[i], x2[i], x3[i], x4[i], x5[i]) - y[i]) * x5[i]
+
+    theta_0_updated *= 2/n
+    theta_1_updated *= 2/n
+    theta_2_updated *= 2/n
+    theta_3_updated *= 2/n
+    theta_4_updated *= 2/n
+    theta_5_updated *= 2/n
+
+    theta_0_final = theta_0_current - alpha * theta_0_updated
+    theta_1_final = theta_1_current - alpha * theta_1_updated
+    theta_2_final = theta_2_current - alpha * theta_2_updated
+    theta_3_final = theta_3_current - alpha * theta_3_updated
+    theta_4_final = theta_4_current - alpha * theta_4_updated
+    theta_5_final = theta_5_current - alpha * theta_5_updated
+    
+    return theta_0_final, theta_1_final, theta_2_final, theta_3_final, theta_4_final, theta_5_final
 
 def one_param_gradient_descent(data, starting_theta_0, starting_theta_1, learning_rate, num_iterations):
     # valores iniciais
