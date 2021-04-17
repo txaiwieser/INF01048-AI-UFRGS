532fae36ff80c284f53e73c927f1203af3a1d8b2
matheus rhoden <matheusrhoden@gmail.com>
2021-04-17 17:44:12 -0300

    pass learning rate to gradient functions

--- t1/p3/gradient_based_optimization.py
+++ t1/p3/gradient_based_optimization.py
@@ -220,7 +220,7 @@ def one_param_gradient_descent(data, starting_theta_0, starting_theta_1, learnin
     # Para cada iteração, obtem novos (Theta0,Theta1) e calcula o custo (EQM)
     for i in range(num_iterations):
         cost_graph.append(one_param_compute_cost(theta_0, theta_1, data))
-        theta_0, theta_1 = one_param_step_gradient(theta_0, theta_1, data, alpha=0.0001)
+        theta_0, theta_1 = one_param_step_gradient(theta_0, theta_1, data, learning_rate)
         theta_0_progress.append(theta_0)
         theta_1_progress.append(theta_1)
 
@@ -243,7 +243,7 @@ def two_param_gradient_descent(data, starting_theta_0, starting_theta_1, startin
     # Para cada iteração, obtem novos (Theta0, Theta1) e calcula o custo (EQM)
     for i in range(num_iterations):
         cost_graph.append(two_param_compute_cost(theta_0, theta_1, theta_2, data))
-        theta_0, theta_1, theta_2 = two_param_step_gradient(theta_0, theta_1, theta_2, data, alpha=0.0001)
+        theta_0, theta_1, theta_2 = two_param_step_gradient(theta_0, theta_1, theta_2, data, learning_rate)
         theta_0_progress.append(theta_0)
         theta_1_progress.append(theta_1)
         theta_2_progress.append(theta_2)
@@ -273,7 +273,7 @@ def five_param_gradient_descent(data, starting_theta_0, starting_theta_1, starti
     # Para cada iteração, obtem novos (Theta0, Theta1) e calcula o custo (EQM)
     for i in range(num_iterations):
         cost_graph.append(five_param_compute_cost(theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, data))
-        theta_0, theta_1, theta_2, theta_3, theta_4, theta_5 = five_param_step_gradient(theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, data, alpha=0.0001)
+        theta_0, theta_1, theta_2, theta_3, theta_4, theta_5 = five_param_step_gradient(theta_0, theta_1, theta_2, theta_3, theta_4, theta_5, data, learning_rate)
         theta_0_progress.append(theta_0)
         theta_1_progress.append(theta_1)
         theta_2_progress.append(theta_2)
